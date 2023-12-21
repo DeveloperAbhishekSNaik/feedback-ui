c0a4f88e46ec42aed816bbf2965146e62186bb13
DeveloperAbhishekSNaik <abhisheksatishnaik@gmail.com>
2023-12-21 04:45:32 -0800

    animate and motion changes using framer motion

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import FeedbackItems from './FeedbackItems';
+import {motion,AnimatePresence} from 'framer-motion';
 
 function FeedbackList({feedback,handleDelete}){
    if(!feedback || feedback.length===0){
@@ -6,12 +8,19 @@ function FeedbackList({feedback,handleDelete}){
    }
 
     return(
-        <div className="feedback-list">
+ <div className="feedback-list">
+        <AnimatePresence>
          {   feedback.map((item)  => (
+            <motion.div key={item.id}
+              initial ={{opacity:0 }}
+              animate = {{opacity : 1}}
+              exit = {{opacity : 0}} >
                 <FeedbackItems key={item.id} item={item} handleDelete={handleDelete}/>
+         </motion.div>
             ))}
-        </div>
-    )
+     </AnimatePresence>
+</div>
+)
 }
 
 export default FeedbackList ;
